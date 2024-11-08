5ff32a2b3363c78665744dad0cdc240a3183befa
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-11-08 23:16:32 +0300

    Add models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from app.models import Application, Applicant, Document, Payment, Operator, Administrator
 
-# Register your models here.
+
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'phone', 'application_status']
+    search_fields = ['first_name', 'last_name', 'phone']
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'submission_date', 'status', 'applicant', 'payment', 'operator']
+    search_fields = ['status']
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'doc_type', 'file', 'application']
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'amount', 'payment_date', 'status', 'application']
+
+
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'access_level']
+    search_fields = ['fist_name', 'last_name']
+
+
+class AdministratorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'access_level']
+    search_fields = ['first_name', 'last_name']
+
+
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(Applicant, ApplicantAdmin)
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Operator, OperatorAdmin)
+admin.site.register(Administrator, AdministratorAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class Applicant(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(max_length=100, blank=False, null=False)
+    phone = models.CharField(max_length=20, blank=False, null=False)
+    application_status = models.OneToOneField('Application', on_delete=models.SET_NULL, null=True, blank=True,
+                                              related_name='applicant_status')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Application(models.Model):
+    id = models.AutoField(primary_key=True)
+    STATUSES = (
+        ('In processing', 'In processing'),
+        ('Approved', 'Approved'),
+    )
+    submission_date = models.DateTimeField(blank=False, null=False, default=timezone.now)
+    status = models.CharField(max_length=20, choices=STATUSES, blank=False, null=False)
+    documents = models.ManyToManyField('Document', blank=True, related_name='applications')
+    applicant = models.OneToOneField(Applicant, on_delete=models.CASCADE, related_name='application')
+    payment = models.OneToOneField('Payment', on_delete=models.CASCADE, null=True, blank=True,
+                                   related_name='application_payment')
+    operator = models.OneToOneField('Operator', on_delete=models.CASCADE, null=True, blank=True,
+                                    related_name='application_operator')
+
+    def __str__(self):
+        return f"Application {self.id} - Status: {self.status}"
+
+
+class Document(models.Model):
+    id = models.AutoField(primary_key=True)
+    doc_type = models.CharField(max_length=100, blank=False, null=False)
+    file = models.FileField(upload_to='documents/')
+    application = models.ForeignKey(Application, on_delete=models.CASCADE, related_name='related_documents')
+
+    def __str__(self):
+        return f"Document {self.id} - Type: {self.doc_type}"
+
+
+class Payment(models.Model):
+    id = models.AutoField(primary_key=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=50, blank=False, null=False)
+    application = models.OneToOneField(Application, on_delete=models.CASCADE,
+                                       related_name='payment_details')
+
+    def __str__(self):
+        return f"Payment {self.id} - Amount: {self.amount}"
+
+
+class Operator(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(max_length=100, blank=False, null=False)
+    access_level = models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return f"Operator {self.id} - {self.first_name} {self.last_name}"
+
+
+class Administrator(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(max_length=100, blank=False, null=False)
+    access_level = models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return f"Administrator {self.id} - {self.first_name} {self.last_name}"
